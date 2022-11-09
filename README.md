9275298b3c7fd512de3b2cb69d59192bc7978f3e
Alex Hsieh <alexhsieh12@gmail.com>
2022-11-09 02:09:39 -0800

    push

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# Note-Taker-3
+ 
+## Note Taker 
+    
+## Description
+This github repository is able to take into account the different notes that a user needs to input and write down. It also lets the user delete their notes after they have finished using it all stored through the json database.
+        
+## Table of Contents
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [License](#license)
+* [Contributing](#how-to-contribute)
+* [Tests](#tests)
+* [Questions?](#questions)
+    
+## Installation
+To install, make sure to download the entire github package and utilize the server.js file to initiate the server. It will then allow you to host the server locally and access it.
+            
+## Usage
+
+It will be usable after installing node and express. 
+    
+## License
+The source code for this repository is licensed under the Eclipse Public License 1.0, which can be found under the license.txt file.
+    
+## How to Contribute
+
+    
+## Tests
+I have tested adding notes, deleting notes, as well as having current notes show up dynamically where users have left off.
--- server.js
+++ server.js
@@ -1,1 +1,54 @@
-hello
+const express = require('express');
+const fs = require('fs');
+const app = express();
+const path = require('path');
+const { stringify } = require('querystring');
+const PORT = process.env.PORT || 3001;
+const { v4: uuidv4 } = require('uuid');
+const notesFeedback = require('./db/db.json')
+// const index = require('./public/assets/js/index')
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+// app.use(bodyParser.urlencoded({extended : true}))
+
+//send the request to fetch the index.html page on load of port
+app.get('/', (req,res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// send request to fetch notes.html when clicking get startd
+app.get('/notes', (req,res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.get('/api/notes', (req,res) => {
+    fs.readFile('./db/db.json', 'utf-8', function(err,data) {
+        // res.send(uuidv4())
+        console.log(data)
+    })
+    res.json(notesFeedback)
+});
+
+app.post('/api/notes', (req,res) => {
+    req.body.id = uuidv4()
+    notesFeedback.push(req.body)
+    fs.writeFileSync('./db/db.json', JSON.stringify(notesFeedback))
+    res.json(notesFeedback)
+})
+
+app.delete('/api/notes/:id', (req,res) => {
+    const id = req.params.id;
+    for (let i = 0; i < notesFeedback.length; i++) {
+        if(notesFeedback[i].id == id) {
+            notesFeedback.splice(i, 1)
+            // console.log(notesFeedback[i])
+            fs.writeFileSync('./db/db.json', JSON.stringify(notesFeedback))
+            res.json(notesFeedback)
+        }
+    }
+})
+
+app.listen(PORT, () =>
+    console.log(`listening for requests on port http://localhost:${PORT}`)
+);
